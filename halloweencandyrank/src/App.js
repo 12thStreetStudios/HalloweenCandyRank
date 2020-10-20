177ffffbe10fa4faadfcf1a14dbca6ec4631e10c
invention64 <jimmygottshall@gmail.com>
2020-10-20 17:32:20 -0400

    App js

--- halloweencandyrank/src/App.js
+++ halloweencandyrank/src/App.js
@@ -1,16 +1,39 @@
-import React from 'react';
-import './components';
+import React, { Component } from 'react';
 import './App.css';
 import Candy from './components/Candy';
 
-export default function App() {
+class App extends Component {
+  state = {
+    data: null
+  };
+
+  componentDidMount() {
+    this.callBackendAPI()
+      .then(res => this.setState({ data: res.express}))
+      .catch(err => console.log(err));
+  }
+
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+    return body;
+  };
+
+  render() {
   return (
     <div className="App">
+      <p>{this.state.data}</p>
       <div className="Candy">
         <Candy className="CandyOne" name="test1"/>
         <Candy className="CandyTwo" name="test2"/>
       </div>
 
     </div>
-  );
+    );
+  }
 }
+
+export default App;
