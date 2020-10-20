5e1ef54e886f64e25e938b54cc4ed30f3bdc5bc4
Russell <rdaswimmer@gmail.com>
2020-10-20 17:16:38 -0400

    edited the components

--- halloweencandyrank/src/App.js
+++ halloweencandyrank/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import './components';
-import './App.css';
 import Candy from './components/Candy';
+import Skip from './components/Skip';
+
+import './styles/App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div className="Candy">
-        <Candy className="CandyOne" name="test1"/>
-        <Candy className="CandyTwo" name="test2"/>
+        <ul>
+          <Candy className="CandyOne" name="test1"/>
+          <Candy className="CandyTwo" name="test2"/>
+        </ul>
+        <Skip />
       </div>
 
     </div>
--- halloweencandyrank/src/components/Skip.js
+++ halloweencandyrank/src/components/Skip.js
@@ -5,12 +5,17 @@ export default class Skip extends Component {
     
     onClick = () => {
         /* To be implemented */
+        
     }
 
     render(){
         return (
             <div className="Skip">
-                <h3>Hello, World!</h3>
+                <div className="Button" onClick={this.onClick}>
+                    <h3>
+                     Press Here To Skip    
+                    </h3>
+                </div>
             </div>
         );
     }
