5aac5de8ed63a4a653738a0e9e8ed6bcced6e3c1
invention64 <jimmygottshall@gmail.com>
2020-10-20 18:04:11 -0400

    mysql

--- halloweencandyrank/server.js
+++ halloweencandyrank/server.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+  host: 'localhost',
+  user: 'candyuser',
+  password: 'candypass'
+});
+
+async function asyncFunction() {
+  let conn;
+  try {
+    conn = await SecurityPolicyViolationEvent.getConnection();
+    const rows = await conn.query("SELECT 1 as val");
+    console.log(rows);
+    const res = await  conn.query("SELECT * FROM candy;");
+    console.log(res);
+  } catch (err) {
+    throw err;
+  } finally {
+    if (conn) return conn.end();
+  }
+}
+
 
 // log that server is up
 app.listen(port, () => console.log(`Listening on port ${port}`));
