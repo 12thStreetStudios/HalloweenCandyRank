61a0c7cc0318bb507032aacd2acbafc670ff6d22
Russell <rdaswimmer@gmail.com>
2020-10-20 17:29:10 -0400

    added div's

--- halloweencandyrank/src/App.js
+++ halloweencandyrank/src/App.js
@@ -9,12 +9,18 @@ export default function App() {
     <div className="App">
       <div className="Candy">
         <ul>
+        <div className="Candy1">
           <Candy className="CandyOne" name="test1"/>
+        </div>
+        <div>
           <Candy className="CandyTwo" name="test2"/>
+        </div>
         </ul>
+      </div>
+      
+      <div className="SkipButton">
         <Skip />
       </div>
-
     </div>
   );
 }
