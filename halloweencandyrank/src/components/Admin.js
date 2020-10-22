57f64dd5b67d03594d83eb7d5ac48a5e63c34aef
Russell <rdaswimmer@gmail.com>
2020-10-22 18:12:59 -0400

    added admin component and css

---
+++ halloweencandyrank/src/components/Admin.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import '../styles/Admin.css';
+
+export default class Admin extends Component {
+    /* Admin page that displays admin features such as:
+        ==> moderation features
+    */
+}
